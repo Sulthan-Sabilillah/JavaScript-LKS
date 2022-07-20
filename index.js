b4fc5426e21ea7c7f6e7fe816aa4c616d0919ece
Sulthan <sulthan.sabilillah@gmail.com>
2022-07-20 13:27:53 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hi Thole", "Doing Something New");
+console.log("Hi Thole");
+
+/*
+console.log("Hi Thole");
+console.log("Hi Thole");
+// console.log("Hi Thole");
+
+
+console.log("Hi Thole");
